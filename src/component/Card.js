4de7d3d4c307d60acb186ad779f59bedf9028d73
mr-dargi <dargimohammadreza@gmail.com>
2023-07-17 19:25:22 +0330

    Adding badgetext to card component and make it dynamic, if the activites was sold out or was online it will show it in our card

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,13 +2,22 @@ import React from "react";
 
 
 export default function Card(props) {
+
+  let badgeTxt;
+  if(props.item.openSpots === 0) {
+    badgeTxt = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeTxt = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {badgeTxt !== undefined && <div className="card-badge">{badgeTxt}</div>}
       <img src={"./images/" + props.item.coverImg} className="card-img" />
       <div className="stat">
         <img src="./images/star.png" className="star" />
         <span>{props.item.stats.rating}</span>
-        <span>({props.item.stats.reviewCount}) . USA</span>
+        <span>({props.item.stats.reviewCount}) . {props.item.location}</span>
       </div>
       <h5>{props.item.title}</h5>
       <p>
